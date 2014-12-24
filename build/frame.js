b1b60d08818a196ea4e150c36cd3106eadf0c3a6
David Glivar <davidglivar@gmail.com>
2014-12-24 18:06:58 -0500

    test stubs

--- build/frame.js
+++ build/frame.js
@@ -7,25 +7,75 @@ var frame = {
 
   framerate: 59,
 
-  add: function (func) {},
+  add: function (func, scope) {
+    if (typeof func !== "function") {
+      throw new TypeError("Expected a function, got " + typeof func);
+    }
+    if (this.exists(func)) return false;
+    func.__scope = scope || func;
+    stack.push(func);
+    return true;
+  },
 
   clearInterval: function (id) {},
 
   clearTimeout: function (id) {},
 
-  exists: function (func) {},
+  exists: function (func) {
+    return stack.indexOf(func) > -1;
+  },
 
-  remove: function (func) {},
+  remove: function (func) {
+    if (typeof func !== "function") {
+      throw new TypeError("Expected a function, got " + typeof func);
+    }
+    if (!this.exists(func)) return false;
+    stack.splice(stack.indexOf(func), 1);
+    return true;
+  },
 
   setInterval: function (func, int) {},
 
   setTimeout: function (func, delay) {},
 
-  start: function () {},
+  start: function () {
+    var _this = this;
+    if (!stopped) return false;
+    stopped = false;
+    then = started = window.performance.now();
+    interval = 1000 / this.framerate;
+    rid = window.requestAnimationFrame(function (time) {
+      return _this.update(time);
+    });
+    return true;
+  },
 
-  stop: function () {},
+  stop: function () {
+    if (stopped) return false;
+    stopped = true;
+    window.cancelAnimationFrame(rid);
+    return true;
+  },
 
-  update: function () {}
+  update: function (time) {
+    var _this2 = this;
+    if (stopped) return;
+
+    window.requestAnimationFrame(function (t) {
+      return _this2.update(t);
+    });
+
+    now = time;
+    delta = now - then;
+    if (delta >= interval) {
+      then = now - (delta % interval);
+      this.fps = Math.round(1000 / ((now - started) / ++frameCount) * 100) / 100;
+      for (var _iterator = stack[Symbol.iterator](), _step; !(_step = _iterator.next()).done;) {
+        var func = _step.value;
+        func.call(func.__scope);
+      }
+    }
+  }
 
 };
 
--- test/frame.test.js
+++ test/frame.test.js
@@ -1,3 +1,60 @@
 var test = require('tape')
-  , frame = require('../index');
+  , frame = require('../index')
+  , sinon = require('sinon');
 
+function pending(t) {
+  t.fail('PENDING');
+  t.end();
+}
+
+test('add', function (t) {
+  t.plan(4);
+  t.ok('add' in frame, 'exists');
+  t.test('throws a TypeError if argument is not a function', pending);
+  t.test('returns false if function already exists in call stack', pending);
+  t.test('returns true if function has been added to call stack', pending);
+});
+
+test('clearInterval', pending);
+
+test('clearTimeout', pending);
+
+test('exists', function (t) {
+  t.plan(3);
+  t.ok('exists' in frame, 'exists');
+  t.test('returns false if function is not found in call stack', pending);
+  t.test('returns true if function is found in call stack', pending);
+});
+
+test('remove', function (t) {
+  t.plan(4);
+  t.ok('remove' in frame, 'exists');
+  t.test('throws a TypeError if argument is not a function', pending);
+  t.test('returns false if function is not in the call stack', pending);
+  t.test('returns true if function is found and removed from call stack', pending);
+});
+
+test('setInterval', pending);
+
+test('setTimeout', pending);
+
+test('start', function (t) {
+  t.plan(3);
+  t.ok('start' in frame, 'exists');
+  t.test('returns false if frame is not stopped', pending);
+  t.test('returns true if frame started successfully', pending);
+});
+
+test('stop', function (t) {
+  t.plan(3);
+  t.ok('stop' in frame, 'exists');
+  t.test('returns false if frame is stopped', pending);
+  t.test('returns true if frame stopped successfully', pending);
+});
+
+test('update', function (t) {
+  t.ok('update' in frame, 'exists');
+  t.test('does not run stack if stopped', pending);
+  t.test('runs stack if not stopped', pending);
+  t.test('calculates fps', pending);
+});
